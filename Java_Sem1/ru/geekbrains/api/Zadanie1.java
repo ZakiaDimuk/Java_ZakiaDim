cb23e89d5ece01343365a31203492adc6eddcf6b
ZakiaDimuk <dimukasheva97@mail.ru>
2023-07-02 18:23:06 +0500

    Zadanie1

---
+++ Java_Sem1/ru/geekbrains/api/Zadanie1.java
@@ -0,0 +1,19 @@
+// 1) Вычислить n-ое треугольного число (сумма чисел от 1 до n), n! (произведение чисел от 1 до n)
+package ru.geekbrains.api;
+
+import java.util.Scanner;
+public class Zadanie1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите число n: ");
+        int n = scanner.nextInt();
+        long sumN = 0;
+        long proizvN = 1;
+        for (int i = 1; i <= n; i++) {
+            sumN = sumN + i;
+            proizvN = proizvN * i;
+        }
+        System.out.println("Сумма первых n чисел: " + sumN);
+        System.out.println("Произведение n чисел: " + proizvN);
+    }
+}
